fafa70274455d9a3cd5ef14836c39d72fb877d93
Sridhar Sarnobat <ss401533@gmail.com>
2023-06-28 18:50:14 -0700

    Minor changes (untested)

---
+++ sum_even_numbers.py
@@ -0,0 +1,11 @@
+#!/Volumes/numerous/usr/local/homebrew/bin//python3
+
+def sum_of_evens(nums):
+# 	println("hello world")
+	# There are smarter ways of doing this but I
+	# want to practice the fundamentals of python syntax
+	for i in nums:
+		print(i)
+
+
+sum_of_evens([1,2,3,4,5,6,7])
