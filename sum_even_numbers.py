be594d7bc61f6e95bfd364873343fc0d90a69d17
Sridhar Sarnobat <ss401533@gmail.com>
2023-06-28 18:50:30 -0700

    Minor changes (untested)

--- sum_even_numbers.py
+++ sum_even_numbers.py
@@ -5,7 +5,8 @@ def sum_of_evens(nums):
 	# There are smarter ways of doing this but I
 	# want to practice the fundamentals of python syntax
 	for i in nums:
-		print(i)
+		if i % 2 == 0:
+			print(i)
 
 
 sum_of_evens([1,2,3,4,5,6,7])
