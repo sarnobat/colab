37de8aa04f80a03c52b2698b7d8949ec3eebe572
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-13 21:26:48 -0700

    Minor changes (untested)

--- plot_rooms.py
+++ plot_rooms.py
@@ -14,13 +14,16 @@ def found_fit(x):
 x_data = list(range(10))
 y_data = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
+# From here the plotting starts
+
+plt.scatter(x_data, y_data, c='r', label='data')
+
+
+
 x_func = np.linspace(0, 10, 50)
 # numpy will do the right thing and evaluate found_fit for all elements
 y_func = found_fit(x_func)
 
-# From here the plotting starts
-
-plt.scatter(x_data, y_data, c='r', label='data')
 plt.plot(x_func, y_func, label='$f(x) = 0.388 x^2$')
 plt.xlabel('x')
 plt.ylabel('y')
