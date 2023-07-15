8beffa24a22e90ebd8d2a03691f28613f08d17e5
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-15 11:36:45 -0700

    Minor changes (untested)

--- plot_rooms.py
+++ plot_rooms.py
@@ -7,15 +7,21 @@ import pandas as pd
 
 ##
 df = pd.read_csv(sys.argv[1] if len(sys.argv) > 1 else 'housing.csv')
+# print(df.columns[1:2].values[:,None])
+print(df)
+print(df['total_rooms'].values)
+print(df['median_house_value'].values)
+plt.scatter(df['total_rooms'].values, df['median_house_value'].values, c='r', label='data2')
 
 def found_fit(x):
-    return 0.388 * x**2  # Found with symfit.
+	return 1502560.535 -1927.793779 * x + 0.9177545759 * x**2 -0.00009422419816 * x**3 
+#     return 0.388 * x**2  # Found with symfit.
 
 x_data = list(range(10))
 y_data = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 plt.scatter(x_data, y_data, c='r', label='data')
 
-x_func = np.linspace(0, 10, 50)
+x_func = np.linspace(0, 1000, 5000, 20000)
 y_func = found_fit(x_func)
 
 plt.plot(x_func, y_func, label='$f(x) = 0.388 x^2$')
