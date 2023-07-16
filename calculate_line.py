2e6e4a7da7547ae7cdf825cbdafcf36e6db4ed2e
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-15 23:10:55 -0700

    Minor changes (untested)

--- calculate_line.py
+++ calculate_line.py
@@ -11,24 +11,33 @@ df = pd.read_csv(sys.argv[1] if len(sys.argv) > 1 else 'housing.csv')
 # print(df)
 column_name = 'total_rooms'
 column_y_name = 'median_house_value'
+
+print('\n=== Input ===')
 print(df[column_name].values)
 print(df[column_y_name].values)
 plt.scatter(df[column_name].values, df[column_y_name].values, c='r', label='data2')
 
-print('\n=== Sample ===')
+
 number_of_features = 3
 print(df.head(number_of_features)[column_name].values)
+
+
+
+df['total_rooms_squared'] = df.total_rooms * df.total_rooms
+df['total_rooms_cubed'] = df.total_rooms * df.total_rooms * df.total_rooms
+
 
 # https://numpy.org/doc/stable/reference/random/generated/numpy.random.random.html
 # a = np.random.random(5000000)
 # b = np.random.random(5000000)
-a = np.array([[1,2,3],[4,5,6],[4,5,6],[1,1,1]])
+a = np.array([
+	[1,1,1],
+	[1,2,3],
+	[4,5,6],
+	[4,5,6]])
 # b = np.array([[1,4,5,6]])
 
 x = a
-
-
-
 
 print('\n=== A ===')
 print(a)
