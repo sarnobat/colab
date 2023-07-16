a7ccc93a4118cd261306a58b6173e159fdefefb2
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-15 20:23:41 -0700

    Minor changes (untested)

--- calculate_line.py
+++ calculate_line.py
@@ -14,10 +14,26 @@ print(df['median_house_value'].values)
 plt.scatter(df['total_rooms'].values, df['median_house_value'].values, c='r', label='data2')
 
 
-a = np.random.random(5000000)
-b = np.random.random(5000000)
-print(np.array(a).dot(np.array(b)))
+# https://numpy.org/doc/stable/reference/random/generated/numpy.random.random.html
+# a = np.random.random(5000000)
+# b = np.random.random(5000000)
+a = np.array([1,2,3])
+b = np.array([4,5,6])
 
+x = a
+xTranspose = np.matrix.transpose(x)
+
+xTransposeDotX = xTranspose.dot(x)
+
+print('-----------------')
+print(a)
+print('-----------------')
+print(b)
+print('-----------------')
+print(np.array(a).dot(np.array(b)))
+print('-----------------')
+print(xTransposeDotX)
+print('-----------------')
 
 x = np.linspace(0, 1000, 5000, 20000)
 fx = lambda x:  1502560.535 -1927.793779 * x + 0.9177545759 * x**2 -0.00009422419816 * x**3
