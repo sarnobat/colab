a888ffc10a709f029cc276d1aa38a41c5d32370d
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-15 22:22:27 -0700

    Minor changes (untested)

--- calculate_line.py
+++ calculate_line.py
@@ -9,10 +9,10 @@ import pandas as pd
 df = pd.read_csv(sys.argv[1] if len(sys.argv) > 1 else 'housing.csv')
 # print(df.columns[1:2].values[:,None])
 print(df)
-print(df['total_rooms'].values)
+column_name = 'total_rooms'
+print(df[column_name].values)
 print(df['median_house_value'].values)
 plt.scatter(df['total_rooms'].values, df['median_house_value'].values, c='r', label='data2')
-
 
 # https://numpy.org/doc/stable/reference/random/generated/numpy.random.random.html
 # a = np.random.random(5000000)
@@ -33,23 +33,24 @@ xTranspose = np.matrix.transpose(x)
 
 print('=== X Transposed ===')
 print(xTranspose)
+
 print('=== X Transposed dot X ===')
 xTransposeDotX = xTranspose.dot(x)
 print(xTransposeDotX)
 
 print('=== inverse (X Transposed dot X) ===')
 xTransposeDotXInverted = np.linalg.inv(xTransposeDotX)
-
 # print('-----------------')
 # print(xTransposeDotX)
 # print('-----------------')
 print(xTransposeDotXInverted)
+
 print('=== inverse (X Transposed dot X) dot Y===')
 y = np.array([7,8,9])
 theta = xTransposeDotXInverted.dot(y)
 print(theta)
-print('-----------------')
 
+print('-----------------')
 x = np.linspace(0, 1000, 5000, 20000)
 fx = lambda x:  1502560.535 -1927.793779 * x + 0.9177545759 * x**2 -0.00009422419816 * x**3
 
